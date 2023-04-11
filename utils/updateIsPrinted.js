7c3ab34ed3a512aa7cf0fb16a09509998f3b9cfa
dulekt <dulekt@gmail.com>
2023-04-11 14:04:51 +0200

    console log updateIsPrinted

--- utils/updateIsPrinted.js
+++ utils/updateIsPrinted.js
@@ -7,6 +7,7 @@ module.exports = {
 async function updateIsPrinted(id) {
     try {
         const updateOrderTrue = await query('UPDATE orders SET "isPrinted" = true WHERE id = $1', [id]);
+        console.log('Marked as done');
     } catch (error) {
         console.log('error: ', error);
     }
