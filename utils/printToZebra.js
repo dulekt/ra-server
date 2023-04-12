8ce1ec4701c77645d1007215d28b06aacbd865c7
dulekt <dulekt@gmail.com>
2023-04-12 14:21:45 +0200

    turn off printing

--- utils/printToZebra.js
+++ utils/printToZebra.js
@@ -17,10 +17,13 @@ function printToZebra(ipAddress, port, zpl) {
     }
 }
 
+{
+    /*
 const zpl =
     '^XA^CFE,23^CFE,23^XA^CFE,23^FO8,27^FB203,0,1,C^FD-SE^FS^FO233,27^FB203,0,1,C^FD-SE^FS^FO458,27^FB203,0,1,C^FD-SE^FS ^XZ^XA^MMC^XZ';
 
 const port = 9100;
 const ipAddress = '10.76.13.150';
-printToZebra(ipAddress, port, zpl);
+printToZebra(ipAddress, port, zpl); */
+}
 // production labels 10.76.14.29 and 10.76.14.30
