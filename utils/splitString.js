c43bbeb6f4a66da72896e31dc7c0907c31013c68
dulekt <dulekt@gmail.com>
2023-07-15 10:07:11 +0200

    printToZebra updated

---
+++ utils/splitString.js
@@ -0,0 +1,4 @@
+// function takes a string and splits it into an array of 3 strings where delimiter is either _ or -
+// check what would be the most even way of splitting the string
+// and return the array of 3 strings
+function splitString(str) {
